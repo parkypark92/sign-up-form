c90453511e2a597c5696ba555ad3617e00ae7dbd
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-12-15 18:14:54 +0000

    add visual password constraints, which turn green when met and are otherwise red if the user is typing password

--- script.js
+++ script.js
@@ -5,18 +5,73 @@ const telephone = document.getElementById('tel');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('password-confirm');
 
+const minCharacter = document.getElementById('min-char');
+const oneLetter = document.getElementById('one-letter');
+const oneNumber = document.getElementById('one-number');
+
+
 let inputs = [firstName, lastName, email, telephone, password, confirmPassword];
 
-inputs.forEach(element => {
-  element.addEventListener('input', () => {
-    element.className="";
-    if(element.value === "") {
+let passCheck = [minCharacter, oneLetter, oneNumber];
+
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    input.className="";
+    if(input.value === "") {
         return;
     }
-    else if(element.checkValidity()) {
-        element.classList.add('valid');
+    else if(input.checkValidity()) {
+        input.classList.add('valid');
     } else {
-        element.classList.add('invalid');
+        input.classList.add('invalid');
     }
 })  
 });
+
+password.addEventListener('input', () => {
+    passCheck.forEach(check => {
+        check.className = "";
+    })
+    letterCheck(password);
+    numberCheck(password);
+    minimumCharacterCheck(password);
+})
+
+
+function letterCheck(userPassword) {
+    if(userPassword.value == "") {
+        return;
+    }
+    else if(/[a-zA-Z]/.test(userPassword.value)) {
+        oneLetter.classList.add('pass-check-green');
+    }
+    else {
+        oneLetter.classList.add('pass-check-red');
+    }
+}
+
+
+function numberCheck(userPassword) {
+    if(userPassword.value == "") {
+        return;
+    }
+    else if(/[0-9]/.test(userPassword.value)) {
+        oneNumber.classList.add('pass-check-green');
+    }
+    else {
+        oneNumber.classList.add('pass-check-red');
+    }
+}
+
+
+function minimumCharacterCheck(userPassword) {
+    if(userPassword.value == "") {
+        return;
+    }
+    else if(/.{8,}/.test(userPassword.value)) {
+        minCharacter.classList.add('pass-check-green');
+    }
+    else {
+        minCharacter.classList.add('pass-check-red');
+    }
+}
